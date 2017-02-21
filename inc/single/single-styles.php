51ca118be43124b2fb1d57e7950d7a2d3ef5e719
Madars Bitenieks <madza@madzathemes.com>
2017-02-21 14:44:01 +0200

    fix

--- inc/single/single-styles.php
+++ inc/single/single-styles.php
@@ -177,6 +177,7 @@
         				</div>
         				<div class="col-md-12">
         				</div>
+                <?php $copyright = get_post_meta(get_the_ID(), "magazin_img_copyright", true); if(!empty($copyright)){ ?><span class="mt-img-copyright"><?php echo $copyright; ?></span><?php } ?>
               </div>
             </div>
     			</div>
