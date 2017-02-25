d58c1ed119b3b6b18b56275cc04b2e1df08a8253
Madars Bitenieks <madza@madzathemes.com>
2017-02-25 04:55:01 +0200

    fix

--- inc/js/defer.js
+++ inc/js/defer.js
@@ -1,3 +1,20 @@
+var today = new Date();
+var h = today.getHours();
+function startTime() {
+    var today = new Date();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time-live').innerHTML =
+    h + ":" + m + "<span>:" + s + "</span>";
+    var t = setTimeout(function(){requestAnimationFrame(startTime)}, 1000);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
 // Hide Header on on scroll down
 var didScroll;
 var lastScrollTop = 0;
--- inc/js/effects.js
+++ inc/js/effects.js
@@ -1,20 +1,3 @@
-var today = new Date();
-var h = today.getHours();
-function startTime() {
-    var today = new Date();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('time-live').innerHTML =
-    h + ":" + m + "<span>:" + s + "</span>";
-    var t = setTimeout(function(){requestAnimationFrame(startTime)}, 1000);
-}
-function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-}
-
 jQuery(document).ready(function() {
   'use strict';
 
